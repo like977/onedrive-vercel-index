4f07cf4956f1ed86c99835881b65024eb0c1425c
Liam <43336441+like977@users.noreply.github.com>
2022-12-02 18:54:18 +0800

    Update api.config.js

--- config/api.config.js
+++ config/api.config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
   clientId: '7a95feb4-e7fe-4063-850e-0b50e6b45bed',
-  obfuscatedClientSecret: 'U2FsdGVkX182iGAlwEnGGzygdmuv7IEXElWuB/lrv+yA44LAa1UnUGVTvCMYosc+bMrP25IrV0rpJjdCxrow3A==',
+  obfuscatedClientSecret: 'U2FsdGVkX1/JlMfILNHHsX9cxTTxFsDawTn+i2eerynhJZ9VNyf+IZR1Nu0kP6Wkwz/J3+sWBm2uS+AVR4Tvcw==',
   redirectUri: 'http://localhost',
   authApi: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
   driveApi: 'https://graph.microsoft.com/v1.0/me/drive',
